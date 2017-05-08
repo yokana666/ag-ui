a8a017ae7facca3c821e2f3e745c1312b3ec6956
indriHutabalian <indri.hutabalian@moonlay.com>
2017-05-08 14:36:16 +0700

    update list report

--- src/modules/power-bi/purchasing/list.js
+++ src/modules/power-bi/purchasing/list.js
@@ -7,14 +7,14 @@ import {Service} from './service';
 export class List {
     data = [];
     listReport = [
-        "63193d44-40f7-4983-ae54-1f4096fed943",//Durasi PR
-        "163de3e2-fe44-4f12-9177-625c15fb976f",//Durasi PO Internal - PO Eksternal
-        "49aac9e1-0abd-4f4a-b6fe-23c4813cc118",//Durasi PO Eksternal - Surat Jalan
-        "3d33db62-883e-4e15-8a25-07cedd32dba1",//Total Nilai Pembelian
-        "1031ae03-7b0d-4466-928b-e59e3a24c39d",//Supplier Tepat Waktu
-        "89ee506a-cf54-48d1-92ca-d2a0b9de26fd",//Perbandingan Purchase Order
-        // "2b455aa7-9d41-4c0c-883b-730454e428cd",//Top Ten Supplier per Total SPB
-        "4fd55640-57bd-46b5-bf9c-b584869a31fd", //Total Hutang
+        "AG Durasi PR",
+        "AG Durasi PO Internal PO Eksternal",
+        "AG Durasi PO Eksternal Surat Jalan",
+        "AG Total Pembelian",
+        "AG Total Hutang",
+        "AG Perbandingan Purchase Order",
+        "AG Ketepatan Waktu Supplier",
+
     ];
     constructor(router, service) {
         this.router = router;
@@ -25,7 +25,7 @@ export class List {
         this.service.search('')
             .then(data => {
                 for (var report of this.listReport) {
-                    var _data = data.find((_data) => _data.id === report);
+                    var _data = data.find((_data) => _data.name === report);
                     if (_data) {
                         this.data.push(_data);
                     }
