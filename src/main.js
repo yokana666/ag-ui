95442c025282900c77bf4e0171bb943236afb7ed
danliris <tris.setiawan@moonlay.com>
2018-07-31 21:25:15 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,10 +30,10 @@ export async function configure(aurelia) {
         }
       }
       
-      var core = "https://ag-core-api-dev.mybluemix.net/v1/";
-      var auth = "https://ag-auth-api-dev.mybluemix.net/v1/";
+      var core = "https://ag-core-api-dev.azurewebsites.net/v1/";
+      var auth = "https://ag-auth-api.azurewebsites.net/v1/";
       var production = "https://ag-production-webapi-dev.mybluemix.net/v1/";
-      var purchasing = "https://ag-purchasing-webapi-dev.mybluemix.net/v1/";    
+      var purchasing = "https://ag-purchasing-webapi-dev.azurewebsites.net/v1/";    
       
       config.registerEndpoint('auth', auth);
       config.registerEndpoint('core', core);
